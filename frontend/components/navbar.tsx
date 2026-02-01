a0acf3a8badf1b4d4ea60547df8c8747e134836f
Marcus <marcus.doberl@gmail.com>
2026-02-01 16:26:35 +0100

    Minor ui improvments

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
@@ -23,46 +24,24 @@ import { ThemeSwitch } from "@/components/theme-switch";
 import { SemelCreatorModal } from "@/components/SemelCreatorModal";
 import { useSemelContext } from "@/app/SemelProvider";
 import {
-  TwitterIcon,
   GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
   Logo,
 } from "@/components/icons";
+import Semel from "../public/favicon.ico"
 
 export const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { refreshSemels } = useSemelContext();
 
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+
 
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <Image src={Semel} alt="Semel Logo" width={32} height={32} />
+            <p className="font-bold text-inherit">Semel Rater</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -93,8 +72,6 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-      
         <NavbarItem>
           <Button
             color="primary"
@@ -122,7 +99,6 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
